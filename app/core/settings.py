e0c65dd7de21d218b8607dc541e004c5118304fb
Álvaro Kayc <155195361+alvarokaycss@users.noreply.github.com>
2026-02-04 16:52:15 -0300

    chore: setup core implementação infraestrutura

---
+++ app/core/settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Settings(BaseSettings):
+    """
+    Configurações da aplicação.
+    """
+
+    API_V1_STR: str = "/api/v1"
+    DB_URL: str
+    DBBaseModel = declarative_base()
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()

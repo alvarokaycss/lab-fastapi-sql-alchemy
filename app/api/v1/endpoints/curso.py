057b658f9b381b1b2ca1e3c28acfbd95b0a5c072
Álvaro Kayc <155195361+alvarokaycss@users.noreply.github.com>
2026-02-05 23:55:41 -0300

    feat: implementação CRUD curso (GET, POST, PUT, DELETE)

---
+++ app/api/v1/endpoints/curso.py
@@ -0,0 +1,108 @@
+# Futuro: Separar em uma arquitetura MVC adaptada para o FastAPI
+
+from typing import List
+
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from fastapi import Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import text
+
+from models.curso_model import CursoModel
+from schemas.curso_schema import CursoSchema
+from core.settings import get_session
+
+
+router = APIRouter(prefix='/cursos', tags=['cursos'])
+
+
+# Testar conexão com banco
+@router.get("/teste-db", status_code=status.HTTP_200_OK)
+async def testar_db(db: AsyncSession = Depends(get_session)):
+    try:
+        await db.execute(text("SELECT 1"))
+        return {"message": "Conexão com banco de dados estabelecida com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Erro ao conectar ao banco de dados: {e}")
+    
+    
+# POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+async def post_curso(curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(
+        titulo=curso.titulo,
+        aulas=curso.aulas,
+        horas=curso.horas
+    )
+
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
+
+
+# GET cursos
+@router.get("/", response_model=List[CursoSchema])
+async def get_cursos(db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel)
+        result = await session.execute(query)
+        cursos: List[CursoModel] = result.scalars().all()
+
+        return cursos
+
+
+# GET curso
+@router.get("/{curso_id}", response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso: CursoModel = result.scalar_one_or_none()
+        
+        if curso is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+        
+        return curso
+    
+
+# PUT curso
+@router.put("/{curso_id}", response_model=CursoSchema, status_code=status.HTTP_202_ACCEPTED)
+async def put_curso(curso_id: int, curso: CursoModel, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_up: CursoModel = result.scalar_one_or_none()
+        
+        if curso_up is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+        
+        curso_up.titulo = curso.titulo
+        curso_up.aulas = curso.aulas
+        curso_up.horas = curso.horas
+        
+        await session.commit()
+        
+        return curso_up
+    
+    
+# DELETE curso
+@router.delete("/{curso_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del: CursoModel = result.scalar_one_or_none()
+        
+        if curso_del is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+        
+        await session.delete(curso_del)
+        await session.commit()
+    
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
